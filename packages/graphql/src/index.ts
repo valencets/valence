1169445dd7604e0284c626cdc2b0308b8cfc6aba
Forrest Blade <mail@forrestblade.com>
2026-03-20 11:17:49 -0500

    REFACTOR: VAL-134 Phase 2 export generateGraphQLSchema from package barrel
    Also exports singularize and capitalize helpers from schema-generator.

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -1,3 +1,4 @@
 // @valencets/graphql — GraphQL schema generation from CMS collection configs
 
 export { fieldToGraphQLType } from './type-builder.js'
+export { generateGraphQLSchema, singularize, capitalize } from './schema-generator.js'
